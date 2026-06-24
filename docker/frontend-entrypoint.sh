d61ad66b84e4beffe62c4302274c0a9fdafcfa7f
ilyanozary <ilyanozary.dynamic@gmail.com>
2026-06-24 15:19:27 +0330

    add sh front docker

--- docker/frontend-entrypoint.sh
+++ docker/frontend-entrypoint.sh
@@ -3,8 +3,9 @@ set -e
 
 if [ ! -x node_modules/.bin/next ]; then
   echo "Frontend dependencies are missing in mounted node_modules; restoring from image cache..."
-  rm -rf node_modules
-  cp -a /opt/frontend_node_modules node_modules
+  mkdir -p node_modules
+  find node_modules -mindepth 1 -maxdepth 1 -exec rm -rf {} +
+  cp -a /opt/frontend_node_modules/. node_modules/
 fi
 
 exec "$@"
