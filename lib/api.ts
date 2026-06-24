fbba3d042ac828ac663a1afbf0275fb396c7253a
ilyanozary <ilyanozary.dynamic@gmail.com>
2026-06-24 13:39:53 +0330

    change port

--- lib/api.ts
+++ lib/api.ts
@@ -1,4 +1,4 @@
-export const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000";
+export const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000";
 
 export function getToken() {
   if (typeof window === "undefined") return null;
